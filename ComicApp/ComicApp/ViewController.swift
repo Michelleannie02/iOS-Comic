01df2f735c3f583393ec259a6ad36d99589f7268
Le Van Anh <lvanh@Les-iMac.local>
2019-12-08 02:41:44 +0700

    init project 1

---
+++ ComicApp/ComicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComicApp
+//
+//  Created by Le Van Anh on 12/8/19.
+//  Copyright © 2019 Le Van Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
